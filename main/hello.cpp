f2b47073053387ba39ccfefdedd617af997e7726
Rakshith Shenoy <rakshithshenoy@outlook.com>
2023-02-16 10:13:42 +0530

    make another change

--- main/hello.cpp
+++ main/hello.cpp
@@ -8,6 +8,6 @@ int main(void)
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Adding another commit";
+  cout << "Adding another commit 2";
   return 0;
 }
